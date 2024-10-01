f26b0cf811363ddc56e8cfedad1525fcfc82b5b5
VKudr <v.kudraivzev@yandex.ru>
2024-10-01 20:11:54 +0300

    dz2

---
+++ src/main/java/vtb/learn/dz2/model/Person.java
@@ -0,0 +1,11 @@
+package vtb.learn.dz2.model;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+    private Integer age;
+    private String jobTitle;
+}
